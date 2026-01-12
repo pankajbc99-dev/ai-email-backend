8bba466a6d54610da4f4be27479cf1c5470e1181
pankajbc99-dev <pankaj.bc99@gmail.com>
2026-01-12 23:27:56 +0530

    Add GET route for Generate API

--- routes/generate.js
+++ routes/generate.js
@@ -2,6 +2,21 @@ const express = require("express");
 const router = express.Router();
 const { generateEmail } = require("../controllers/aiController");
 
+router.get("/", (req, res) => {
+  res.send("Generate API is working. Use POST request.");
+});
+
+router.post("/", generateEmail);
+
+module.exports = router;
+const express = require("express");
+const router = express.Router();
+const { generateEmail } = require("../controllers/aiController");
+
+router.get("/", (req, res) => {
+  res.send("Generate API is working. Use POST request.");
+});
+
 router.post("/", generateEmail);
 
 module.exports = router;
