25369842af3136a5a0de5f2cb0206972b0a6f8d3
pankajbc99-dev <pankaj.bc99@gmail.com>
2026-01-12 23:52:38 +0530

    Remove GET route and clean up generate.js

--- routes/generate.js
+++ routes/generate.js
@@ -2,21 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { generateEmail } = require("../controllers/aiController");
 
-router.get("/", (req, res) => {
-  res.send("Generate API is working. Use POST request.");
-});
-
-router.post("/", generateEmail);
-
-module.exports = router;
-const express = require("express");
-const router = express.Router();
-const { generateEmail } = require("../controllers/aiController");
-
-router.get("/", (req, res) => {
-  res.send("Generate API is working. Use POST request.");
-});
-
 router.post("/", generateEmail);
 
 module.exports = router;
